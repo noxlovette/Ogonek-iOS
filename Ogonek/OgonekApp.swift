85d49bd0437d0cbf928f9ffc5ee6de5b7c3c869d
Danila Volkov <noxlovette@proton.me>
2025-07-14 10:12:48 +0300

    sentry init

--- Ogonek/OgonekApp.swift
+++ Ogonek/OgonekApp.swift
@@ -6,9 +6,37 @@
 //
 
 import SwiftUI
+import Sentry
+
 
 @main
 struct Ogonek: App {
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://f6266ebea7566596d2e3ea95739037dc@o4507272574468096.ingest.de.sentry.io/4509665650737232"
+            options.debug = true // Enabled debug when first installing is always helpful
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 1.0 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+        }
+        // Remove the next line after confirming that your Sentry integration is working.
+        SentrySDK.capture(message: "This app uses Sentry! :)")
+    }
     var body: some Scene {
         @State var lessonProvider = LessonProvider()
         WindowGroup {
